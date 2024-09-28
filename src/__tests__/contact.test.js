2ae070f1c4db0c6848b373f8eee11f2575c2fb3a
rinshadcp <rinshadcp7337@gmail.com>
2024-09-28 12:35:46 +0530

    added test cases for Contact Component

--- src/__tests__/contact.test.js
+++ src/__tests__/contact.test.js
@@ -2,9 +2,21 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test("Should render the contact in dom", () => {
-  render(<Contact />);
+describe("Contact component related test cases", () => {
+  it("Should render the contact in dom", () => {
+    render(<Contact />);
 
-  const item = screen.getAllByRole("textbox");
-  expect(item.length).toBe(2);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("Should 2 textbox(input) in the Contact Component", () => {
+    render(<Contact />);
+
+    const textboxes = screen.getAllByRole("textbox");
+
+    //assertion
+
+    expect(textboxes.length).toBe(2);
+  });
 });
